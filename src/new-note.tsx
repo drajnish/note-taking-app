b3ca3176a14007e98d266b2a825ea538073063f4
Rajnish Deshmukh <rajnishdeshmukh05@gmail.com>
2024-08-23 18:59:50 +0530

    initial setup and libraries like bootstrap and react-select installed

---
+++ src/new-note.tsx
@@ -0,0 +1,12 @@
+import NoteForm from './note-form';
+
+function NewNote() {
+  return (
+    <>
+      <h1 className="mb-4">New Note</h1>
+      <NoteForm />
+    </>
+  );
+}
+
+export default NewNote;
